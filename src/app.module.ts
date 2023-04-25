81f8a5c8bedef5ef969e28557f55b3dbade71d31
sniffjoke <sniffjoke@gmail.com>
2023-04-25 22:38:26 +0200

    end of project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PostModule } from './post/post.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { join } from 'path'
 
 @Module({
-  imports: [PostModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'crash-course-nestjs-red-group',
+      username: 'postgres',
+      password: '12345',
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      migrations: [join(__dirname, '**', '*.migration.{ts,js}')],
+      synchronize: true
+    }),
+    PostModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,37 +1,41 @@
 import { Body, Delete, Get, Injectable, Param, Post, Put } from '@nestjs/common'
 import { CreatePostDto } from './post.dto/create-post.dto'
+import { InjectRepository } from '@nestjs/typeorm'
+import { PostEntity } from './post.entity'
+import { Repository } from 'typeorm'
 
 @Injectable()
 export class PostService {
-  posts: any[]
-
-  constructor() {
-    this.posts = [
-      { id: 1, content: 'rest' },
-      { id: 2, content: 'rest' },
-      { id: 3, content: 'rest' }
-    ]
-  }
+  constructor(
+    @InjectRepository(PostEntity)
+    private readonly postRepository: Repository<PostEntity>
+  ) {}
 
   async getAll() {
-    return this.posts
+    return this.postRepository.find()
   }
 
   async create(dto: CreatePostDto) {
-    return [...this.posts, dto]
+    const post = this.postRepository.create(dto)
+    return await this.postRepository.save(post)
   }
 
   async getById(id: string) {
-    return this.posts.find(p => p.id === Number(id))
+    return this.postRepository.findOne({
+      where: {
+        id: Number(id)
+      }
+    })
   }
 
   async update(id: string, dto: CreatePostDto) {
-    const post = await this.posts.find(p => p.id === Number(id))
+    const post = await this.getById(id)
     post.content = dto.content
-    return post
+    post.userName = dto.userName
+    return await this.postRepository.save(post)
   }
 
   async delete(id: string) {
-    return this.posts.filter(p => p.id !== id)
+    return await this.postRepository.delete({ id: Number(id) })
   }
 }
